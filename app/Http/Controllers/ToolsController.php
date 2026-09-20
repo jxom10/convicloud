c39e3c011e9baa420b1fba37b21e9602dfe38d84
jxom10 <pascalbouix@hotmail.com>
2026-09-20 23:40:32 +0200

    reparado update

--- app/Http/Controllers/ToolsController.php
+++ app/Http/Controllers/ToolsController.php
@@ -9,35 +9,31 @@ use Illuminate\Support\Facades\Process;
 class ToolsController extends Controller
 {
    public function actualizar(Request $request){
-
 		if(isset($request->password)) {
 			
 			
 			if($request->password=='Iesmh4ever'){	
-				$pasos = array("crear carpeta temporal"=>"mkdir .convicloud_temp",
-						"crear carpeta temporal"=>"cd .convicloud_temp",
-						"descargar actualizacion"=>"wget https://github.com/jxom10/convicloud/archive/refs/heads/main.zip",
-						"descomprimir fichero"=>"unzip -q main.zip",
-						"cambiar permisos public"=>"chmod 777 -R convicloud-main/public",
-						"cambiar permisos storage"=>"chmod 777 -R convicloud-main/storage",
-						"actualizar ficheros"=>"rsync -av convicloud-main/. ../../",
-						"borrar carpeta temporal"=>"rm -R .convicloud_temp");
-						
-				foreach($pasos as $desc=>$script){
-					if(!$result = Process::run($script)){
-						echo "error en ".$desc."<br>";
-						print_r($result);
-					}
-				}
-			
-			session()->flash('message', ['texto'=>'Actualizacion correcta','color'=>'success']);
-			return view('inicio');
+
+				$result = Process::run('mkdir .convicloud_temp');
+				$result = Process::path('.convicloud_temp')->run("wget https://github.com/jxom10/convicloud/archive/refs/heads/main.zip");
+				$result = Process::path('.convicloud_temp')->run("unzip -q main.zip");
+				$result = Process::path('.convicloud_temp')->run("chmod 777 -R .convicloud_temp/convicloud-main/public");
+				$result = Process::path('.convicloud_temp')->run("chmod 777 -R .convicloud_temp/convicloud-main/storage");
+				$result = Process::path('.convicloud_temp')->run("rsync -avi convicloud-main/. ../../");
+				echo "<pre>";
+				print_r($result->output());
+				echo "</pre>";
+				$result = Process::run('rm -R .convicloud_temp');
+				session()->flash('message', ['texto'=>'Actualizacion correcta','color'=>'success']);
+				die();
+				//return view('inicio');
 
 			}
 			else{
 				session()->flash('message', ['texto'=>'La contraseña no es válida','color'=>'warning']);
 			}
 		}
+
 		return view('tools.update');	
 	}
 
